8f28b57a487fcd92b3e12096349a2115d28afb09
Mika <cornelis.m@outlook.com>
2022-12-14 14:01:13 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+var santas = document.getElementsByClassName('poli')
+
+var snow = document.querySelector('.gif')
+
+
+let n = 0;
+
+while (n < 3) {
+    
+   
+    santas.style.transform = 'rotate(90deg)';
+
+}
